4acc9a6c1e0f3d5ee8c7503c2d7d1c11a2a2191d
Noel Cothren <noelcothren@gmail.com>
2017-02-12 11:56:33 -0700

    added webpack/index.php

---
+++ webpack/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+
+		<title>Angular 2 Experiment</title>
+	</head>
+	<body>
+		<angular2-experiment>Loading&hellip;</angular2-experiment>
+	</body>
+</html>
